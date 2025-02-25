15a11ad807004ed4144a6aad8fd3de9adcaa4578
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2025-02-25 16:48:12 +0000

    home intro cards grid

--- page-templates/blocks/cb_home_intro_cards.php
+++ page-templates/blocks/cb_home_intro_cards.php
@@ -9,22 +9,28 @@ $l3 = get_field('card3_link') ?? null;
         <img src="<?=get_stylesheet_directory_uri()?>/img/home_intro_lines.svg" alt="" class="home_intro__lines d-none d-md-block">
         <div class="row g-5">
             <div class="col-md-4 text-center">
-                <img src="<?=get_field('card1_icon')?>" alt="" class="home_intro__icon">
-                <h2 class="h4 has-purple-400-color"><?=get_field('card1_title')?></h2>
-                <div class="fs-300 mb-4"><?=get_field('card1_content')?></div>
-                <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                <div class="home_intro__card">
+                    <img src="<?=get_field('card1_icon')?>" alt="" class="home_intro__icon">
+                    <h2 class="h4 has-purple-400-color"><?=get_field('card1_title')?></h2>
+                    <div class="fs-300 mb-4"><?=get_field('card1_content')?></div>
+                    <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                </div>
             </div>
             <div class="col-md-4 text-center">
-                <img src="<?=get_field('card2_icon')?>" alt="" class="home_intro__icon">
-                <h2 class="h4 has-teal-400-color"><?=get_field('card2_title')?></h2>
-                <div class="fs-300 mb-4"><?=get_field('card2_content')?></div>
-                <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                <div class="home_intro__card">
+                    <img src="<?=get_field('card2_icon')?>" alt="" class="home_intro__icon">
+                    <h2 class="h4 has-teal-400-color"><?=get_field('card2_title')?></h2>
+                    <div class="fs-300 mb-4"><?=get_field('card2_content')?></div>
+                    <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                </div>
             </div>
             <div class="col-md-4 text-center">
-                <img src="<?=get_field('card3_icon')?>" alt="" class="home_intro__icon">
-                <h2 class="h4 has-amber-400-color"><?=get_field('card3_title')?></h2>
-                <div class="fs-300 mb-4"><?=get_field('card3_content')?></div>
-                <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                <div class="home_intro__card">
+                    <img src="<?=get_field('card3_icon')?>" alt="" class="home_intro__icon">
+                    <h2 class="h4 has-amber-400-color"><?=get_field('card3_title')?></h2>
+                    <div class="fs-300 mb-4"><?=get_field('card3_content')?></div>
+                    <a href="<?=$l1['url']?>" target="<?=$l1['targt']?>" class="button button--sm"><?=$l1['title']?></a>
+                </div>
             </div>
         </div>
         <img src="<?=get_stylesheet_directory_uri()?>/img/home_intro_brace.svg" alt="" class="home_intro__brace">
