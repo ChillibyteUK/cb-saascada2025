aed56edebede313f21c15713a95a715b77a65d11
ChillibyteUK <dan@chillibyte.co.uk>
2025-03-27 11:11:10 +0000

    index

--- index.php
+++ index.php
@@ -15,9 +15,6 @@ get_header();
                 <div class="page_hero__content"><a href="/contact/" class="button button-outline">Contact us</a></div>
         </div>
     </section>
-
-
-
     <div class="container-xl py-5 news">
         <?php
         if (get_the_content(null, false, $page_for_posts)) {
