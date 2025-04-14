9d33048cd9d270c6be547c2acd15596d1b9d700c
ChillibyteUK <dan@chillibyte.co.uk>
2025-04-14 08:29:32 +0000

    404

--- 404.php
+++ 404.php
@@ -24,7 +24,7 @@ $bg = wp_get_attachment_image_url( get_field( 'hero_image', 'options' ), 'full' 
                 <h1 class="mb-4">404 - Page Not Found</h1>
                 <div class="hero__content fs-5 mb-4">We can't seem to find the page you're looking for</div>
                 <div class="hero__cta">
-                    <a class="btn btn-default--secondary mb-4" href="/">Return to Homepage</a>
+                    <a class="button button-primary mb-4" href="/">Return to Homepage</a>
                 </div>
             </div>
             <div class="col-lg-6 hero__image order-1 order-lg-2" style="background-image:url(<?= esc_url( $bg ); ?>)">
