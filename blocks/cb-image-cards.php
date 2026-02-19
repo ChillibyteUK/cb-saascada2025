dbf42516a71c78506d5b5c23b40d75380da7e0b7
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-19 12:29:40 +0000

    Image cards

--- blocks/cb-image-cards.php
+++ blocks/cb-image-cards.php
@@ -10,6 +10,8 @@ $card_gap        = get_field( 'card_gap' ) ?? 'g-5';
 $block_padding   = get_field( 'block_padding' ) ?? 'py-5';
 $col_class       = 'col-md-' . ( 12 / $number_of_cards );
 
+$image_size = $number_of_cards <= 2 ? 'large' : 'full';
+
 // Support Gutenberg color picker.
 $bg = ! empty( $block['backgroundColor'] ) ? 'has-' . $block['backgroundColor'] . '-background-color' : '';
 $fg = ! empty( $block['textColor'] ) ? 'has-' . $block['textColor'] . '-color' : '';
@@ -23,24 +25,32 @@ $fg = ! empty( $block['textColor'] ) ? 'has-' . $block['textColor'] . '-color' :
 				the_row();
 				?>
 			<div class="<?= esc_attr( $col_class ); ?>">
-				<?php
-				$image = get_sub_field( 'image' );
-				if ( $image ) {
-					echo wp_get_attachment_image( $image, 'full', false, array( 'class' => 'image-cards__image' ) );
-				}
-				?>
-				<h3><?= esc_html( get_sub_field( 'title' ) ); ?></h3>
-				<div><?= wp_kses_post( get_sub_field( 'content' ) ); ?></div>
-				<?php
-				$card_link = get_sub_field( 'link' );
-				if ( $card_link ) {
-					?>
-					<a href="<?= esc_url( $card_link['url'] ); ?>" class="btn btn-primary" target="<?= esc_attr( $card_link['target'] ); ?>">
-						<?= esc_html( $card_link['title'] ); ?>
-					</a>
+				<div class="image-cards__card">
 					<?php
-				}
-				?>
+					$image = get_sub_field( 'image' );
+					if ( $image ) {
+						?>
+					<div class="image-cards__image-wrapper">
+						<?= wp_get_attachment_image( $image, $image_size, false, array( 'class' => 'image-cards__image' ) ); ?>
+					</div>
+						<?php
+					}
+					?>
+					<div class="image-cards__content">
+						<h2><?= esc_html( get_sub_field( 'title' ) ); ?></h2>
+						<div><?= wp_kses_post( get_sub_field( 'content' ) ); ?></div>
+						<?php
+						$card_link = get_sub_field( 'link' );
+						if ( $card_link ) {
+							?>
+							<a href="<?= esc_url( $card_link['url'] ); ?>" class="button button--sm w-100" target="<?= esc_attr( $card_link['target'] ); ?>">
+								<?= esc_html( $card_link['title'] ); ?>
+							</a>
+							<?php
+						}
+						?>
+					</div>
+				</div>
 			</div>
 				<?php
 			}
