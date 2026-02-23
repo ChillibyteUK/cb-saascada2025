ffd676f09b14a019e4beac5d2ac656df79c1aab6
ChillibyteUK <dan@chillibyte.co.uk>
2026-02-23 10:26:12 +0000

    text image size

--- blocks/cb-text-image.php
+++ blocks/cb-text-image.php
@@ -43,7 +43,7 @@ $section_id = $block['anchor'] ?? null;
 
 $image_id = get_field( 'image' );
 
-$image_size = $split === '60:40' ? 'full' : 'large';
+$image_size = $split === '40:60' ? 'large' : 'full';
 
 if ( $image_id ) {
     $img = wp_get_attachment_image( $image_id, $image_size, false, array( 'class' => 'text_image__img' ) );
